79cc5cb9b4d9588f25e53504e2a733f7a93b9b98
Lisa Marín <39717147+lisa07system@users.noreply.github.com>
2019-10-01 20:34:12 -0300

    archivos sql del Modelo.libro (#23)
    * archivos sql del Modelo.libro
    
    * reemplazo del codigo por archivos .sql

--- src/main/java/modelo/Libro.java
+++ src/main/java/modelo/Libro.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import util.ManejadorDeArchivos;
+
 import java.util.ArrayList;
 
 public class Libro implements IModelo {
@@ -53,44 +55,71 @@ public class Libro implements IModelo {
         this.Prestable = Prestable;
     }
 
+    public String prepararInsert() {
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String sql = ma.abrirArchivo("plantillas/nuevo_libro.sql");
+        sql = sql.replace("{TITULO}", this.Titulo);
+        sql = sql.replace("{TEMA}", Integer.toString(this.Tema));
+        return sql;
+    }
+
     @Override
     public void insert() {
-        String insert = "INSERT INTO `biblioteca`.`libro`"
-                + "	(`Titulo`, `Tema_idTema`, `Ubicacion_id_ubicacion`, `Prestable`)"
-                + "VALUES"
-                + "	('" + this.Titulo + "', " + this.Tema + ",1,1);";
+        String insert = this.prepararInsert();
         CONECTOR.ejecutarSentencia(insert);
+    }
+
+    public String prepararUpdate(String identificador) {
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String sql = ma.abrirArchivo("plantillas/update_libro.sql");
+        sql = sql.replace("{TITULO}", this.Titulo);
+        sql = sql.replace("{ID_TEMA}", Integer.toString(this.Tema));
+        sql = sql.replace("{IDENTIFICADOR}", identificador);
+        return sql;
     }
 
     @Override
     public int update(String identificador) {
-        String update= "UPDATE libro SET Titulo = '"+this.Titulo+"',"
-                + "Tema_idTema = "+this.Tema
-                + " WHERE idLibro = "+identificador+";";
-        
-        System.out.println(update);
+        String update = this.prepararUpdate(identificador);
         return CONECTOR.ejecutarSentencia(update);
+    }
+
+    public String prepararSelectTodos() {
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String sql = ma.abrirArchivo("plantillas/selectTodos_libro.sql");
+        return sql;
     }
 
     @Override
     public ArrayList selectTodos() {
-        String select = "SELECT `idlibro`, `Titulo` FROM `libro` WHERE libro.Prestable = 1 ORDER BY `Titulo` ASC;";
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String select = ma.abrirArchivo("plantillas/selectTodos_libro.sql");
         return CONECTOR.ejecutarConsulta(select);
     }
 
+    public String prepararContarLibros(String idlibro) {
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String sql = ma.abrirArchivo("plantillas/contar_libros.sql");
+        sql = sql.replace("{ID_LIBRO}", idlibro);
+        return sql;
+    }
+
     public int contarLibros(String idlibro) {
-        String select = "SELECT count(*) FROM `codLibro` "
-                + "INNER JOIN libro ON codlibro.idlibro = libro.idlibro "
-                + "WHERE libro.`idlibro` = "+idlibro+" AND libro.Prestable = 1";
+        String select = this.prepararContarLibros(idlibro);
         ArrayList cantidadLibros = CONECTOR.ejecutarConsulta(select);
         return Integer.parseInt(((ArrayList) cantidadLibros.get(0)).get(0).toString());
     }
 
+    public String prepararComprobarExistenciaDeRegistro(String[] data) {
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String sql = ma.abrirArchivo("plantillas/comprobarExistenciaDeRegistro_libro.sql");
+        sql = sql.replace("{DATA}", data[1]);
+        return sql;
+    }
+
     @Override
     public int comprobarExistenciaDeRegistro(String[] data) {
-        String lo = "SELECT idlibro"
-                + " FROM libro"
-                + " WHERE libro.Titulo = \"" + data[1] + "\";";
+        String lo = this.prepararComprobarExistenciaDeRegistro(data);
         ArrayList existencia = CONECTOR.ejecutarConsulta(lo);
         if (existencia.isEmpty()) {
             return 0;
@@ -103,10 +132,18 @@ public class Libro implements IModelo {
         String select = query;
         return CONECTOR.ejecutarConsulta(select);
     }
-    public ArrayList selectUno(String id){
-        String select = "SELECT idLibro, Titulo ,Tema_idTema FROM libro WHERE idLibro = "+id+";";
+
+    public String prepararSelectUno(String id) {
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String sql = ma.abrirArchivo("plantillas/selectUno_libro.sql");
+        sql = sql.replace("{ID_LIBRO}", id);
+        return sql;
+    }
+
+    public ArrayList selectUno(String id) {
+        String select = this.prepararSelectUno(id);
         ArrayList existencia = CONECTOR.ejecutarConsulta(select);
-        return (ArrayList) existencia.get(0); 
+        return (ArrayList) existencia.get(0);
     }
 
     @Override
@@ -114,16 +151,22 @@ public class Libro implements IModelo {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    public String prepararUpdatePrestable(int idExistencia, int valor) {
+        ManejadorDeArchivos ma = new ManejadorDeArchivos();
+        String sql = ma.abrirArchivo("plantillas/updatePrestable_libro.sql");
+        sql = sql.replace("{ID_EXISTENCIA}", Integer.toString(idExistencia));
+        sql = sql.replace("{VALOR}", Integer.toString(valor));
+        return sql;
+    }
+
     public void updatePrestable(int idExistencia, int valor) {
-        String update= "UPDATE libro SET Prestable = "+valor
-                + " WHERE idLibro = "+idExistencia+";";
-        
+        String update = this.prepararUpdatePrestable(idExistencia, valor);
         System.out.println(update);
         CONECTOR.ejecutarSentencia(update);
     }
-    
-    public boolean comprobarPrestamo(String id){
-        String select = "SELECT * FROM `prestamo` WHERE Libro_idlibro = "+id+" AND (Estado_idEstado = 1 OR Estado_idEstado = 2 or Estado_idEstado = 3);";
+
+    public boolean comprobarPrestamo(String id) {
+        String select = "SELECT * FROM `prestamo` WHERE Libro_idlibro = " + id + " AND (Estado_idEstado = 1 OR Estado_idEstado = 2 or Estado_idEstado = 3);";
         ArrayList existencia = CONECTOR.ejecutarConsulta(select);
         return existencia.isEmpty();
     }
